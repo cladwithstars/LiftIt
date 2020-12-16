a3f0bbd9329f26ed25e82957b61f692f8b613d78
Will Morgan <morgan88will@gmail.com>
2020-12-16 09:37:42 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
 app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API....'}));
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false})) //now we can accept body data
 
 //Define Routes
 app.use('/api/users', require('./routes/users'))
